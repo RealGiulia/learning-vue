0aca7335d1b3be9676f71aaa9395aa2566a86ce8
Giulia <giuliaguerreir9@gmail.com>
2024-11-22 20:59:15 -0300

    Insertion of CArIngredientes component

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,13 +1,14 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import Tag from './Tag.vue';
 
 export default {
     data(){
         return{
-            ingredientes: ['Alho','Manteiga','Orégano', 'Tomates']
+            ingredientes: ['Alho','Manteiga','Orégano', 'Tomates', 'Queijo']
         }
     },
-    components: { SelecionarIngredientes }
+    components: { SelecionarIngredientes, Tag }
 }
 </script>
 
@@ -17,7 +18,7 @@ export default {
             <span class="subtitulo-lg sua-lista-texto">Sua lista:</span>
 
             <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes"  :key="ingrediente" class="ingrediente"> {{ ingrediente }}</li>
+                <li v-for="ingrediente in ingredientes"  :key="ingrediente"> <Tag :texto="ingrediente" />  </li>
             </ul>
 
             <p v-else class="paragrafo lista-vazia">
@@ -56,18 +57,6 @@ export default {
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-    transition: 0.2s;
-    color: var(--creme, #FFFAF3);
-  background: var(--coral, #F0633C);
-  font-weight: 700;
 }
 
 .lista-vazia {
--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,38 +1,39 @@
 <script lang="ts">
+import { obterCategorias } from '@/http/index';
+import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CardCategoria.vue';
 
-import { obterCategorias } from "@/http/index";
-
-export default { data(){
+export default {
+  data() {
     return {
-        categorias: []
-        }
-    },
-    async created () {
-        this.categorias = await obterCategorias();
+      categorias: [] as ICategoria[]
     }
- }
-
+  },
+  async created() {
+    this.categorias = await obterCategorias();
+  },
+  components: { CardCategoria }
+}
 </script>
 
-
 <template>
-    <section class="selecionar-ingredientes">
-        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
 
-        <p class="paragrafo-lg instrucoes"> 
-            Selecione abaixo os ingredientes que você quer usar nesta receita:
-        </p>
+    <p class="paragrafo-lg instrucoes">
+      Selecione abaixo os ingredientes que você quer usar nesta receita:
+    </p>
 
-        <ul class="categorias">
-            <li v-for="categoria in categorias" :key="categoria.nome"> {{ categoria.nome }} </li>
+    <ul class="categorias">
+      <li v-for="categoria in categorias" :key="categoria.nome">
+        <CardCategoria :categoria="categoria"/>
+      </li>
+    </ul>
 
-        </ul>
-
-        <p class="paragrafo dica">
-            *Atenção: consideramos que você tenha em casa sal, pimenta e água.
-        </p>
-    </section>
-
+    <p class="paragrafo dica">
+      *Atenção: consideramos que você tem em casa sal, pimenta e água.
+    </p>
+  </section>
 </template>
 
 <style scoped>
